29fb7174843bbd00aec35b1903ece4b0a0f68735
Salome Brack <salome.brack@gmx.de>
2020-10-17 23:31:22 +0200

    Add Database

---
+++ QuizApp/Data/QuizDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.Data
+{
+	public class QuizDbContext : DbContext
+	{
+		public QuizDbContext(DbContextOptions<QuizDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Quiz> Quiz { get; set; }
+	}
+}
