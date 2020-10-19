138e0b139cd9579b10eac7c3913b74144b9f82b6
Salome Brack <salome.brack@gmx.de>
2020-10-19 18:20:34 +0200

    Display Database

--- QuizApp/Controllers/QuizController.cs
+++ QuizApp/Controllers/QuizController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QuizApp.Data;
+using QuizApp.Models;
 
 namespace QuizApp.Controllers
 {
 	public class QuizController : Controller
 	{
+		private readonly QuizDbContext _db;
+
+		public QuizController(QuizDbContext db)
+		{
+			_db = db;
+		}
+
 		public IActionResult Index()
 		{
-			return View();
+			IEnumerable<Quiz> objList = _db.Quiz;
+			return View(objList);
 		}
 	}
 }
